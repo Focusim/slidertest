ea36472a69b9a227a1d0ddc5e296d0fba165f375
Focusim <RoMMik823@mail.ru>
2021-09-09 21:07:24 +0300

    new

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+/* Слайдер на 1+ блоков, 1 из них виден -- начало */
+
+
 /* Устанавливаем стартовый индекс слайда по умолчанию: */
-let slideIndex = 3;
+let slideIndex = 1;
 /* Вызываем функцию, которая реализована ниже: */
 showSlides(slideIndex);
 
@@ -20,14 +23,14 @@ function currentSlide(n) {
 
 /* Функция перелистывания: */
 function showSlides(n) {
-    /* Обращаемся к элементам с названием класса "slider__block", то есть к картинкам: */
+    /* Обращаемся к элементам с названием класса "slider__block" */
     let slides = document.getElementsByClassName("slider__block");
 
     /* Проверяем количество слайдов: */
     if (n > slides.length) {
-        slideIndex = 3
+        slideIndex = 1
     }
-    if (n < 3) {
+    if (n < 1) {
         slideIndex = slides.length
     }
 
@@ -37,10 +40,9 @@ function showSlides(n) {
     }
     /* Делаем элемент блочным: */
     slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex - 2].style.display = "block";
-    slides[slideIndex - 3].style.display = "block";
 }
 
+/* Слайдер на 1+ блоков, 1 из них виден -- конец */
 
 
 
@@ -48,126 +50,106 @@ function showSlides(n) {
 
 
 
+/*Слайдер на 6+ блоков, 3 из них видны -- начало*/
 
-
-
+/*Создаём 3 переменных для применения свойст*/
 number0 = 0
 number1 = 1
 number2 = 2
 
+/*Вызываем функцию 'видимости'*/
 slideView()
 
-//number 0
+/*Пересчитываем блоки слайдера ".top" */
+let slidesTop = document.querySelectorAll(".top")
+
+/*Считаем количество блоков, отнимаем Еденицу для зацикливания перелёстывания слайдов*/
+checkSlide = slidesTop.length - 1
+
+/*Функция клика на кнопку "назад"*/
 function prevTop() {
+    /*Минусуем индекс переменным отталкиваясь от их позиций */
     if (number0 < 1) {
-        number0 = 5
-    } else if (number0 <= 5) {
+        number0 = checkSlide
+    } else if (number0 <= checkSlide) {
         number0 -= 1
     }
     if (number1 < 1) {
-        number1 = 5
-    } else if (number1 <= 5) {
+        number1 = checkSlide
+    } else if (number1 <= checkSlide) {
         number1 -= 1
     }
     if (number2 < 1) {
-        number2 = 5
-    } else if (number2 <= 5) {
+        number2 = checkSlide
+    } else if (number2 <= checkSlide) {
         number2 -= 1
     }
-    slideView()
-    if (number0 === 5) {
+    /*Задаём ордер для правильной номерации блоков при слайде*/
+    if (number0 === checkSlide) {
         let slidesTop = document.querySelectorAll(".top")
         slidesTop.item(0).style.order = "1"
         slidesTop.item(1).style.order = "1"
         slidesTop.item(2).style.order = "1"
     }
+    /*Убираем ордер */
     if (number0 === 2) {
         let slidesTop = document.querySelectorAll(".top")
         slidesTop.item(0).style.order = "unset"
         slidesTop.item(1).style.order = "unset"
         slidesTop.item(2).style.order = "unset"
     }
+    /*Вызываем функцию 'видимости' с изменёными индексамы */
+    slideView()
 }
 
+/*Функция клика на кнопку "вперёд"*/
 function nextTop() {
-    if (number0 < 5) {
+    /*Добавляем индекс переменным отталкиваясь от их позиций */
+    if (number0 < checkSlide) {
         number0 += 1
     } else {
         number0 = 0
     }
-    if (number1 < 5) {
+    if (number1 < checkSlide) {
         number1 += 1
     } else {
         number1 = 0
     }
-    if (number2 < 5) {
+    if (number2 < checkSlide) {
         number2 += 1
     } else {
         number2 = 0
     }
-    slideView()
+    /*Задаём ордер для правильной номерации блоков при слайде*/
     if (number0 === 4) {
         let slidesTop = document.querySelectorAll(".top")
         slidesTop.item(0).style.order = "1"
         slidesTop.item(1).style.order = "1"
         slidesTop.item(2).style.order = "1"
     }
+    /*Убираем ордер */
     if (number0 === 1) {
         let slidesTop = document.querySelectorAll(".top")
         slidesTop.item(0).style.order = "unset"
         slidesTop.item(1).style.order = "unset"
         slidesTop.item(2).style.order = "unset"
-    }
-    console.log(number0) 
+    } 
+    /*Вызываем функцию 'видимости' с изменёными индексамы */
+    slideView()
 }
 
-
-
-
-
-
-
+/*Функция 'видимости' Блоков */
 function slideView() {
+    /*Пересчитываем блоки слайдера ".top" */
     let slidesTop = document.querySelectorAll(".top")
-    console.log(slidesTop)
+    /*Задаём всем блокам 'невидимость' */
     for (let item of slidesTop) {
         item.style.display = "none"
     }
+    /*Даём видимость блокам в зависимости от индексов переменных */
     slidesTop.item(number0).style.display = "block"
     slidesTop.item(number1).style.display = "block"
     slidesTop.item(number2).style.display = "block"
 }
 
-test = 10
-test = test + 10
-console.log(test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log()
-let user = {
-        name: 'Roman',
-        age: '22',
-        sayHi() {
-            //this - текущий обьект
-            alert(this.name)
-        }
-    }
-    //user.sayHi();
+/*Слайдер на 6+ блоков, 3 из них видны -- конец*/
